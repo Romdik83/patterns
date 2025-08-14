63a17720d5079a115e2747e73375988bb9ebc4ab
Romdik <romdik83@gmail.com>
2025-08-14 18:10:24 +0300

    test

--- src/test/java/ReplanDeliveryTest.java
+++ src/test/java/ReplanDeliveryTest.java
@@ -1,2 +1,44 @@
-public class ReplanDeliveryTest {
+import com.codeborne.selenide.SelenideElement;
+import lombok.val;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.Keys;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.Random;
+
+import static com.codeborne.selenide.Condition.*;
+import static com.codeborne.selenide.Selenide.*;
+import static com.codeborne.selenide.Selenide.$;
+
+class ReplanDeliveryTest {
+
+    void dataInput(int days) {
+        SelenideElement data = $("[data-test-id=date]");
+        data.$("[value]").doubleClick().sendKeys(Keys.BACK_SPACE);
+        data.$("[placeholder]").setValue(DataGenerator.dataInput(days));
+    }
+
+    @BeforeEach
+    void befor() {
+        open("http://localhost:9999");
+    }
+
+    @Test
+    void getTrueInputValidForm() {
+        $("[placeholder=Город]").setValue(DataGenerator.cityForInput());
+        int inDays = 4;
+        dataInput(inDays);
+        $("[data-test-id=phone]").$("[name=phone]").setValue(DataGenerator.dataPhone());
+        $("[data-test-id=name].input_type_text .input__control").setValue(DataGenerator.dataName());
+        $("[class=checkbox__box]").click();
+        $$("[class=button__text]").find(exactText("Запланировать")).click();
+        $("[data-test-id=success-notification]").$("[class=notification__content]")
+                .shouldHave(textCaseSensitive("Встреча успешно запланирована на " + DataGenerator.dataInput(inDays)));
+        $$("[class=button__text]").find(exactText("Запланировать")).click();
+        $$("[class=button__text]").find(exactText("Перепланировать")).click();
+        $("[data-test-id=success-notification]").$("[class=notification__content]")
+                .shouldHave(textCaseSensitive("Встреча успешно запланирована на " + DataGenerator.dataInput(inDays)));
+    }
 }
