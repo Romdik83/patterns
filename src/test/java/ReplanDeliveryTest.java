e1263ed17b8985069f9bda58a92019ff5824e0e4
Romdik <romdik83@gmail.com>
2025-08-14 18:22:37 +0300

    test

--- src/test/java/ReplanDeliveryTest.java
+++ src/test/java/ReplanDeliveryTest.java
@@ -25,20 +25,5 @@ class ReplanDeliveryTest {
         open("http://localhost:9999");
     }
 
-    @Test
-    void getTrueInputValidForm() {
-        $("[placeholder=Город]").setValue(DataGenerator.cityForInput());
-        int inDays = 4;
-        dataInput(inDays);
-        $("[data-test-id=phone]").$("[name=phone]").setValue(DataGenerator.dataPhone());
-        $("[data-test-id=name].input_type_text .input__control").setValue(DataGenerator.dataName());
-        $("[class=checkbox__box]").click();
-        $$("[class=button__text]").find(exactText("Запланировать")).click();
-        $("[data-test-id=success-notification]").$("[class=notification__content]")
-                .shouldHave(textCaseSensitive("Встреча успешно запланирована на " + DataGenerator.dataInput(inDays)));
-        $$("[class=button__text]").find(exactText("Запланировать")).click();
-        $$("[class=button__text]").find(exactText("Перепланировать")).click();
-        $("[data-test-id=success-notification]").$("[class=notification__content]")
-                .shouldHave(textCaseSensitive("Встреча успешно запланирована на " + DataGenerator.dataInput(inDays)));
-    }
+
 }
